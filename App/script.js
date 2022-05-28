4cb7112f8fca6f80c945529e87e6656cd0680cb4
Luiz <luizfranzon05@hotmail.com>
2022-05-28 00:37:57 -0300

    Done

---
+++ App/script.js
@@ -0,0 +1,25 @@
+var ReadyToWork = new Audio("assets/ready-to-work.mp3");
+var OutOfGolden = new Audio("assets/out-of-golden.mp3");
+var YesMeLord = new Audio("assets/yes-me-lord.mp3");
+var YourTheKing = new Audio("assets/your-the-king.mp3");
+var MoreWork = new Audio("assets/more-work.mp3");
+
+function playRtw() {
+    ReadyToWork.play();
+}
+
+function playOfG() {
+    OutOfGolden.play();
+}
+
+function playYmL() {
+    YesMeLord.play();
+}
+
+function playYtK() {
+    YourTheKing.play();
+}
+
+function playMw() {
+    MoreWork.play();
+}
